e97b10b2de4c18662412d174eb0400de0248f4f7
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2021-05-06 00:46:26 +0700

    extend interface of application class

--- libraries/core/include/application/ApplicationBase.h
+++ libraries/core/include/application/ApplicationBase.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "tools/TimeState.h"
 #include "SFML/Window/Event.hpp"
 
 class ApplicationDelegate
@@ -6,7 +7,9 @@ class ApplicationDelegate
 public:
 	virtual ~ApplicationDelegate() = default;
 
+	virtual void ProcessCommandLine(int argc, char **argv) = 0;
 	virtual void ProcessEvent(const sf::Event & event) = 0;
+	virtual void ProcessElapsedTime(TimeState elapsedTime) = 0;
 
 	virtual bool ShouldTerminate() const = 0;
 
