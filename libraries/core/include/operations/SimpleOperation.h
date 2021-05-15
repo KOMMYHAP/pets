f5bcfa398bc6731aa77ebd511cfd403b8cf8cba9
Pavel <16599808+KOMMYHAP@users.noreply.github.com>
2021-05-15 22:34:07 +0700

    make constuction of SimpleOperation more flexible: - job - job & completion callback - only completion callback (for timer)

--- libraries/core/include/operations/SimpleOperation.h
+++ libraries/core/include/operations/SimpleOperation.h
@@ -6,9 +6,15 @@ class SimpleOperation : public Operation
 {
 public:
 	SimpleOperation(TypedCallback<> callback);
+	SimpleOperation(TypedCallback<> callback, TypedCallback<std::exception_ptr> onCompleted);
+
+	struct SafeOperationTag {};
+	SimpleOperation(TypedCallback<> onCompleted, SafeOperationTag);
 
 private:
 	Result DoImpl() override;
+	void OnCompleted(std::exception_ptr exception) override;
 	
-	TypedCallback<>			_callback;
+	TypedCallback<>							_callback;
+	TypedCallback<std::exception_ptr>		_onCompleted;
 };
--- libraries/core/src/operations/SimpleOperation.cpp
+++ libraries/core/src/operations/SimpleOperation.cpp
@@ -1,12 +1,40 @@
 #include "operations/SimpleOperation.h"
 
 SimpleOperation::SimpleOperation(TypedCallback<> callback)
-	: _callback(callback)
+	: SimpleOperation(std::move(callback), TypedCallback<std::exception_ptr>())
 {
+}
+
+SimpleOperation::SimpleOperation(TypedCallback<> callback, TypedCallback<std::exception_ptr> onCompleted)
+	: _callback(std::move(callback))
+	, _onCompleted(std::move(onCompleted))
+{
+}
+
+SimpleOperation::SimpleOperation(TypedCallback<> onCompleted, SafeOperationTag)
+{
+	auto owner = onCompleted.Owner();
+	auto onCompletedIgnoringException = [onCompleted = std::move(onCompleted)](std::exception_ptr)
+	{
+		onCompleted();
+	};
+	_onCompleted = TypedCallback<std::exception_ptr>(std::move(owner), std::move(onCompletedIgnoringException));
 }
 
 Operation::Result SimpleOperation::DoImpl()
 {
 	_callback();
 	return Result::Completed;
+}
+
+void SimpleOperation::OnCompleted(std::exception_ptr exception)
+{
+	if (_onCompleted.Valid())
+	{
+		_onCompleted(exception);
+	}
+	else if (exception)
+	{
+		std::rethrow_exception(exception);
+	}
 }
